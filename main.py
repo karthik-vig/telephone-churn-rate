219eefca1099dc81bd312f1fe10125efdaf381f3
Karthik Vignesh Sundar <2649025s@student.gla.ac.uk>
2022-09-14 01:08:23 +0100

    added cnn model for prediction.

--- main.py
+++ main.py
@@ -3,12 +3,118 @@ import os
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 from joblib import dump, load
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
 from sklearn.manifold import TSNE
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+
+
+class DataframeToTensor(Dataset):
+    def __init__(self, x_data_df, y_data_df):
+        self.x_data_df = x_data_df
+        self.y_data_df = y_data_df
+
+    def __len__(self):
+        return len(self.x_data_df)
+
+    def __getitem__(self, index):
+        return torch.tensor([self.x_data_df[index]], dtype=torch.double), \
+               torch.tensor([self.y_data_df[index]], dtype=torch.double)
+
+class DataframeToTensorForPredict(Dataset):
+    def __init__(self, x_data_df):
+        self.x_data_df = x_data_df
+
+    def __len__(self):
+        return len(self.x_data_df)
+
+    def __getitem__(self, index):
+        return torch.tensor([self.x_data_df[index]], dtype=torch.double)
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv1d(1, 3, 3, 2)
+        self.layer1 = nn.Linear(120, 80)
+        self.layer2 = nn.Linear(80, 50)
+        self.layer3 = nn.Linear(50, 30)
+        self.layer4 = nn.Linear(30, 15)
+        self.layer5 = nn.Linear(15, 10)
+        self.layer6 = nn.Linear(10, 5)
+        self.layer7 = nn.Linear(5, 1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool1d(x, kernel_size=2)
+        x = torch.flatten(x, 1)
+        x = self.layer1(x)
+        x = F.relu(x)
+        x = self.layer2(x)
+        x = F.relu(x)
+        x = self.layer3(x)
+        x = F.relu(x)
+        x = self.layer4(x)
+        x = F.relu(x)
+        x = self.layer5(x)
+        x = F.relu(x)
+        x = self.layer6(x)
+        x = F.relu(x)
+        x = self.layer7(x)
+        x = torch.sigmoid(x)
+        return x
+
+    def predict(self, x_data_df):
+        tensor_dataset = DataframeToTensorForPredict(x_data_df=x_data_df)
+        dataloader = DataLoader(tensor_dataset, batch_size=len(x_data_df))
+        for x in dataloader:
+            x = x.to('cuda')
+            x = self.forward(x.double()).cpu().detach().numpy()
+            x = np.where( x > 0.5, 1, 0)
+            x = x.reshape(len(x_data_df))
+            return x
+
+
+def train_cnn(x_data_df, y_data_df, x_test_df, y_test_df):
+    cnn_model = CNN().to('cuda')
+    optimizer = optim.Adam(cnn_model.parameters(), lr=0.0001)
+    loss_fn = nn.BCELoss()
+    tensor_dataset = DataframeToTensor(x_data_df=x_data_df,
+                                       y_data_df=y_data_df)
+    dataloader = DataLoader(tensor_dataset, batch_size=256, shuffle=True)
+    test_acc_list = []
+    cnn_model = cnn_model.double()
+    for epoch in range(1, 50):
+        avg_train_loss = 0
+        for batch, label in dataloader:
+            batch = batch.to('cuda')
+            label = label.to('cuda')
+            output = cnn_model(batch.double())
+            optimizer.zero_grad()
+            loss = loss_fn(output, label)
+            avg_train_loss += loss
+            loss.backward()
+            optimizer.step()
+        print('Epoch: ', epoch, ' Done!')
+        print('Average training loss: ', avg_train_loss / len(x_data_df))
+        cnn_preds = cnn_model.predict(x_test_df)
+        test_acc = accuracy_score(y_test_df, cnn_preds)
+        test_acc_list.append(test_acc)
+    torch.save(cnn_model, 'cnn_model.pt')
+    cnn_preds = cnn_model.predict(x_test_df)
+    print('CNN model accuracy: ', accuracy_score(y_test_df, cnn_preds))
+    plt.close()
+    plt.plot(list(range(1, 50)), test_acc_list)
+    plt.show()
+    return cnn_model
 
 
 def get_data_df(datasplit_type):
@@ -130,8 +236,15 @@ def dataset_process_pipeline(data_df):
     return data_df
 
 
-def get_use_input():
-    user_input = {}
+def get_user_input(test_mode=False, **kwargs):
+    user_input = {'compute_tsne': None,
+                  'load_tsne': None,
+                  'compute_model': None,
+                  'select_model': None}
+    if test_mode:
+        for key, value in kwargs.items():
+            user_input[key] = value
+        return user_input
     compute_tsne_val = int(input('Show t-SNE values? = (1) yes, (2) no: '))
     if compute_tsne_val == 1:
         user_input['compute_tsne'] = True
@@ -148,16 +261,23 @@ def get_use_input():
         user_input['compute_model'] = True
     else:
         user_input['compute_model'] = False
-    select_model_option = int(input('Select a ML model = (1) SVM, (2) Logistic Regression: '))
+    select_model_option = int(input('Select a ML model = (1) SVM, (2) Logistic Regression, (3) CNN: '))
     if select_model_option == 1:
         user_input['select_model'] = 'SVM'
-    else:
+    elif select_model_option == 2:
         user_input['select_model'] = 'log'
+    else:
+        user_input['select_model'] = 'cnn'
+
     return user_input
 
 
 def main():
-    user_input = get_use_input()
+    # user_input = get_user_input(test_mode=True,
+    #                            compute_tsne=False,
+    #                            compute_model=True,
+    #                            select_model='cnn')
+    user_input = get_user_input()
     # process the training dataset and also separate it into training and validation dataset.
     train_df = get_data_df(datasplit_type='train')
     get_data_properties(data_df=train_df)
@@ -183,6 +303,14 @@ def main():
                                           y_test_df=y_val_df)
         else:
             model = load('logreg_model.joblib')
+    elif user_input['select_model'] == 'cnn':
+        if user_input['compute_model']:
+            model = train_cnn(x_data_df=x_train_df,
+                              y_data_df=y_train_df,
+                              x_test_df=x_val_df,
+                              y_test_df=y_val_df)
+        else:
+            model = torch.load('cnn_model.pt')
 
     test_df = get_data_df(datasplit_type='test')
     test_df = dataset_process_pipeline(data_df=test_df)
@@ -193,6 +321,8 @@ def main():
         sol_df.to_csv('svm_solution.csv')
     elif user_input['select_model'] == 'log':
         sol_df.to_csv('logreg_solution.csv')
+    elif user_input['select_model'] == 'cnn':
+        sol_df.to_csv('cnn_solution.csv')
 
 
 if __name__ == '__main__':
